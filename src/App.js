9b0d2412d5f0259181478ebe879779253379f35b
Brandon Turner <brandontheturner@gmail.com>
2019-02-17 13:52:47 -0600

    component practice

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Components/Layout/Header";
+import "./App.css";
+import Todos from "./Components/Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Take Alexis out for dinner",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Go buy a new house",
+        completed: false
+      },
+      {
+        id: 4,
+        title: "Get a great new job",
+        completed: false
+      }
+    ]
+  };
+  // Toggle Complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+    console.log(id);
+  };
+  // Delete Todo
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
+    // console.log(this.state.todos);
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+
+        <Todos
+          todos={this.state.todos}
+          markComplete={this.markComplete}
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
 }
-
 export default App;
